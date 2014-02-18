5fd4721405ac5feab930635df4f6b4dca042ff64
Alexander Mescheryakov <sibsfinx@ya.ru>
2014-02-18 12:23:31 +0400

    projects list icons

--- config/helpers.rb
+++ config/helpers.rb
@@ -48,15 +48,41 @@ module ApplicationHelpers
   end
 
   def device_type(device)
+    device ||= 'web'
     os = ''
     if device.downcase.match(/(iphone|ipad|ipod)/)
       os = 'ios'
     elsif device.downcase.match(/(s3|s4|galaxy|nexus|samsung)/)
       os = 'android'
     else
-      os = 'desktop'
+      os = 'web'
     end
     os
+  end
+
+  def device_type_url project, device
+    device ||= 'web'
+    type = device_type device
+    case type
+      when 'ios'
+        url = project.appstore
+      when 'android'
+        url = project.google_play
+      else
+        url = project.url
+    end
+    url
+  end
+
+  def device_type_link project, device
+    device ||= 'web'
+    url = device_type_url(project, device)
+    icon = content_tag :div, '', class: "project-block-platform #{device_type device}"
+    if url.present?
+      link_to icon, device_type_url(project, device), class: 'device-type-link', target: '_blank'
+    else
+      icon
+    end
   end
 
   def app_badge platform, link
